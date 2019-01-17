adba87f81ae20c92e5ff8091d2f82f1654844544
willnx <willnx84@gmail.com>
2019-01-17 11:46:25 -0800

    added test to catch failure in updating DDNS key

--- run_tests.sh
+++ run_tests.sh
@@ -9,5 +9,6 @@ export VLAB_DNS_FORWARDERS=10.7.190.6,10.7.190.7,
 export VLAB_DNS_REV_ZONE=80.241.10.in-addr.arpa
 export VLAB_IP=10.241.80.12
 export VLAB_FQDN=vlab-dev.igs.corp
+export VLAB_DDNS_KEY=r2a/n4quUpSXzE24p7/KfHSrl0e8SaMcI8jB6bga/7U29vmt9m51oGcL/vTsq9Z93Odx7GiAgcG1vazJbSRq6Q==
 export TESTENV=true
 dns/config_and_run.sh
